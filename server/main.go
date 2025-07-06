d1d78edf62395dbcdca2d0e877e62aa3301f5509
michaszek <punkomichal@gmail.com>
2025-07-07 01:40:09 +0200

    deleted submodule

---
+++ server/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "grpcFirstSteps/proto"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("Server is listening on port %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
